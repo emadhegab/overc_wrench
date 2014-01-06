1317bb40301933c3b352c94023a2a2c6999c4588
Omar Abdel-Wahab <owahab@gmail.com>
2014-01-06 03:14:27 +0200

    Major fix in google_analytics helper

--- app/helpers/overc_wrench_helpers.rb
+++ app/helpers/overc_wrench_helpers.rb
@@ -29,7 +29,7 @@ module OvercWrenchHelpers
 
   # Render Google Analytics JavaScript tag
   # Ideally, should be called in application layout
-  def google_analytics id
+  def google_analytics id, domain
     unless Rails.configuration.consider_all_requests_local
       code = <<-ANALYTICS
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -37,7 +37,7 @@ module OvercWrenchHelpers
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', '#{id}', 'souwrna.herokuapp.com');
+        ga('create', '#{id}', '#{domain}');
         ga('send', 'pageview');
         ANALYTICS
       javascript_tag(code).html_safe
